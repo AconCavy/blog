7e7bc5dd1a334471302d9854647c0286cb722fe7
AconCavy <aconcavy@gmail.com>
2020-11-19 04:54:23 +0900

    Update theme

---
+++ Blog/Extensions/MarkdownExtension.cs
@@ -0,0 +1,31 @@
+﻿using Markdig;
+using Markdig.Renderers;
+using Markdig.Renderers.Html;
+using Markdig.Syntax;
+
+namespace Blog.Extensions
+{
+    public class MarkdownExtension : IMarkdownExtension
+    {
+        public void Setup(MarkdownPipelineBuilder pipeline)
+        {
+            pipeline.DocumentProcessed -= PipelineOnDocumentProcessed;
+            pipeline.DocumentProcessed += PipelineOnDocumentProcessed;
+        }
+
+        public void Setup(MarkdownPipeline pipeline, IMarkdownRenderer renderer)
+        {
+        }
+
+        private static void PipelineOnDocumentProcessed(MarkdownDocument document)
+        {
+            foreach (var node in document.Descendants())
+            {
+                if (node is CodeBlock)
+                {
+                    node.GetAttributes().AddClass("line-numbers");
+                }
+            }
+        }
+    }
+}
--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System.Threading.Tasks;
+using Blog.Extensions;
+using Markdig.Extensions.Bootstrap;
 using Statiq.App;
+using Statiq.Common;
+using Statiq.Markdown;
 using Statiq.Web;
 
 namespace Blog
@@ -9,6 +13,16 @@ namespace Blog
         public static async Task<int> Main(string[] args) =>
             await Bootstrapper.Factory
                 .CreateWeb(args)
+                .ConfigureTemplates(templates =>
+                {
+                    if (!templates.ContainsKey(MediaTypes.Markdown)) return;
+                    templates.Remove(MediaTypes.Markdown);
+                    templates.Add(MediaTypes.Markdown, new Template(ContentType.Content, Phase.Process,
+                        new RenderMarkdown()
+                            .UseExtensions()
+                            .UseExtension<BootstrapExtension>()
+                            .UseExtension<MarkdownExtension>()));
+                })
                 .RunAsync();
     }
 }
