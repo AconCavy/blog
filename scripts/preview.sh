27f8cee22b10f96e80d4223c81fe9668c3181fb1
AconCavy <aconcavy@gmail.com>
2022-12-11 23:34:10 +0900

    Modify the script

--- scripts/preview.sh
+++ scripts/preview.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-dotnet run --project ./src/Blog -- preview
+
+SCRIPT_PATH=$(cd $(dirname $0); pwd)
+ROOT_PATH=$(dirname $SCRIPT_PATH)
+
+dotnet run --project $ROOT_PATH/src/Blog -- preview
